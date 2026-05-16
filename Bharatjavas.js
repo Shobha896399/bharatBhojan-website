7c65512cd734825eda95f85a952b653227f871dd
Shobha896399 <ritikabathaw18@gmail.com>
2026-05-16 20:53:57 +0530

    Add files via upload

---
+++ Bharatjavas.js
@@ -0,0 +1,52 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll =   () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
+
+let header = document.querySelector('header');
+window.addEventListener('scroll',() => {
+    header.classList.toggle('shadow', window.scrollY > 0 );
+});
+
+// script.js
+
+let buttons = document.querySelectorAll(".menu-btn");
+let menuBoxes = document.querySelectorAll(".menu-box");
+
+buttons.forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    // ACTIVE BUTTON REMOVE
+
+    buttons.forEach(btn => {
+      btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+
+    // CATEGORY
+
+    let category = button.getAttribute("data-category");
+
+    // HIDE ALL
+
+    menuBoxes.forEach(box => {
+      box.classList.remove("show");
+    });
+
+    // SHOW SELECTED
+
+    document.querySelector("." + category)
+    .classList.add("show");
+
+  });
+
+});
